9f4ece56fdb96ee9e1a2c033cc3edd0b35d6e6ad
Cosullivan85 <info@cosullivan.dev>
2023-06-04 16:20:26 +0800

    Commit 1

---
+++ main.js
@@ -0,0 +1,16 @@
+import * as THREE from "three";
+
+//Scene
+const scene = new THREE.scene();
+
+//Create our sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, 800, 600);
+scene.add(camera);
